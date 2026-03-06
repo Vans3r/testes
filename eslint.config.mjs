0a65ff1f0f5f2281e017f9e107b31e87506f3bf8
Vans3r <ladno1846@gmail.com>
2026-03-06 23:11:22 +0300

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,19 +3,19 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } }, {
-  "env": {
-    "browser": false,
-    "es2021": true,
-    "node": true
-  },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  },
-  "rules": {
-    "no-unused-vars": "error"
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    extends: [js.configs.recommended],
+    rules: {
+      "no-unused-vars": "error"
+    }
   }
-}
 ]);
